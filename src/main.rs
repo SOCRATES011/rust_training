1b033d40ae86edc8e58aa11efe0b8d434a04df55
Socrates <socratesoduduwa@gmail.com>
2026-07-18 18:00:18 +0100

    ownership scope

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,6 @@
-//First small exercise
+// Ownership
 fn main(){
-    let name = "Dare Akinlade"; // number 1
-    let mut age:u8 = 35; //number 2
-
-    // number 3
-    for number in (1..6).rev(){
-        println!("Number: {} '  ' square of {} is: {}",number,number, number*number);
-    }
-
-    //number 4
-    let status = if age >= 18 {"adult"} else {"minor"};
-    println!(" {} is {}years old therefore, I am an {}",name, age, status);
-
+    let s = String::from("hello"); // s owns the string
+    println!("{}", s);
+    // s goes out of scope here and the memory is freed
 }
