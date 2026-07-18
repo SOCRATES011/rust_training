5d1031f783e5e0ed3c6f89d65191f5a1a36fb05f
Socrates <socratesoduduwa@gmail.com>
2026-07-18 20:35:01 +0100

    Session 2 Exercise: no 4

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,7 @@
 // Session 2 Exercise
 fn main(){
-    let mut student_name = String::from("Pamilerin");
-   change_name(&mut student_name);
-    println!("{}",student_name);
-}
-
-fn change_name(student_name: &mut String){
-    student_name.push_str(" Lawal");
+    let student_name = String::from("Pamilerin");
+    let first_name = &student_name;
+    let last_name = &student_name.clone();
+    println!("my full name is {} {}", first_name, last_name);
 }
