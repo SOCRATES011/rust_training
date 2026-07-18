fc38245377bf42ca3472d8f8da827e239ece1568
Socrates <socratesoduduwa@gmail.com>
2026-07-18 18:06:01 +0100

    move ownership

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-// Ownership
+// Move semantics in Rust means that when a variable is assigned to another variable,
+// the ownership of the value is transferred (moved) to the new variable.
 fn main(){
-    let s = String::from("hello"); // s owns the string
-    println!("{}", s);
-    // s goes out of scope here and the memory is freed
+    let s1 = String::from("hello"); // s1 owns the string
+    let s2 = s1; // ownership of the string is moved to s2
+    //println!("{}", s1); // s2 goes out of scope here and the memory is freed
+    println!("{}", s2);
 }
