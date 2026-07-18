6070886e61dcdd672f7097dce389cada4ba05496
Socrates <socratesoduduwa@gmail.com>
2026-07-18 18:12:59 +0100

    adding copy trait

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,9 @@ fn main() {
     let s1 = String::from("hello");
     let s2 = s1.clone(); // s2 is a deep copy of s1
     println!("s1 = {}, s2 = {}", s1, s2); // both valid
-    
+
+    // Copy trait
+    let x = 5;
+    let y = x; // y is a copy of x
+    println!("x = {}, y = {}", x, y); // both valid
 }
