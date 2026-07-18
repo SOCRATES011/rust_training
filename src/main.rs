cc335a7f8e84e16d9f08b731f5302f5fc8a3d3f6
Socrates <socratesoduduwa@gmail.com>
2026-07-18 19:37:30 +0100

    Session 2 Exercise: no 3

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 // Session 2 Exercise
 fn main(){
-    let student_name = String::from("Pamilerin");
-    let len = calculate_length(&student_name);
-    println!("The length of '{}' is {}.", student_name, len);
+    let mut student_name = String::from("Pamilerin");
+   change_name(&mut student_name);
+    println!("{}",student_name);
 }
 
-fn calculate_length(s: &String) -> usize {
-    s.len()
+fn change_name(student_name: &mut String){
+    student_name.push_str(" Lawal");
 }
